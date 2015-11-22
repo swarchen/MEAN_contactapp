42930c767dd51ffb4a6a9f3d5f0864f3e7e1a5b0
jhaocheng <jhaocheng.wu@gmail.com>
2015-11-22 14:24:22 +0800

    controller

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,3 +1,9 @@
-function AppCtrl (){
+function AppCtrl ($scope, $http){
 	console.log('hello');
+
+	$http.get('/contactlist').success(function(response){
+		console.log('I got the data that I requested!');
+		$scope.contactlist = response;
+	})
+
 }
--- server.js
+++ server.js
@@ -3,5 +3,27 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactlist', function(req, res){
+	console.log('I received a GET request!');
+	person1 = {
+		name: 'Andy',
+		email: 'jhaocheng.wu@gmail.com',
+		number: '0988393123'
+	};
+	person2 = {
+		name: 'John',
+		email: 'John@gmail.com',
+		number: '0911921921'
+	};
+	person3 = {
+		name: 'Lucy',
+		email: 'Lucy@gmail.com',
+		number: '0911921921'
+	};
+
+	var contactlist = [person1, person2, person3];
+	res.json(contactlist);
+});
+
 app.listen(3000);
 console.log('server running port 3000');
