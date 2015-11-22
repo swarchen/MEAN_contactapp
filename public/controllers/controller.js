3314e0eb9924cfdb23138d554b0b2cb326a31930
jhaocheng <jhaocheng.wu@gmail.com>
2015-11-22 14:54:50 +0800

    add post request

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,9 +1,19 @@
 function AppCtrl ($scope, $http){
 	console.log('hello');
-
-	$http.get('/contactlist').success(function(response){
-		console.log('I got the data that I requested!');
-		$scope.contactlist = response;
-	})
+	var refresh = function(){ 
+		$http.get('/contactlist').success(function(response){
+			console.log('I got the data that I requested!');
+			$scope.contactlist = response;
+			$scope.contact = "";
+		})
+	};
+	refresh();
+	$scope.addContact = function(){
+		console.log($scope.contact);
+		$http.post('/contactlist', $scope.contact).success(function(response){
+			console.log(response);
+			refresh();
+		});
+	};
 
 }
--- server.js
+++ server.js
@@ -1,28 +1,26 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist',['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get('/contactlist', function(req, res){
 	console.log('I received a GET request!');
-	person1 = {
-		name: 'Andy',
-		email: 'jhaocheng.wu@gmail.com',
-		number: '0988393123'
-	};
-	person2 = {
-		name: 'John',
-		email: 'John@gmail.com',
-		number: '0911921921'
-	};
-	person3 = {
-		name: 'Lucy',
-		email: 'Lucy@gmail.com',
-		number: '0911921921'
-	};
+	
+	db.contactlist.find(function(err, docs){
+		console.log(docs);
+		res.json(docs);
+	});
+});
 
-	var contactlist = [person1, person2, person3];
-	res.json(contactlist);
+app.post('/contactlist', function(req, res){
+	console.log(req.body);
+	db.contactlist.insert(req.body, function(err, doc){
+		res.json(doc);
+	});
 });
 
 app.listen(3000);
